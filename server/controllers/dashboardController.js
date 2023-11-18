ff28bb2e2e177eb9395a06c814c91aeaa8445f8c
EstebanPastor <estebanandrespastor@gmail.com>
2023-11-18 11:26:11 -0300

    feat: :sparkles: Add pagination to the notes
    Now the user can paginate between the notes added

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,11 +1,48 @@
-exports.dashboard = async (req, res) => {
-    const locals = {
-      title: "Dashboard",
-      description: "Dashboard page",
-    };
-    res.render("dashboard/index", {
-      locals,
-      layout: "../views/layouts/dashboard.ejs"
-    });
+const mongoose = require("mongoose");
+const Note = require("../models/Notes");
+
+exports.dashboard = async (req, res, next) => {
+  const perPage = 8;
+  const page = req.query.page || 1;
+
+  const locals = {
+    title: "Dashboard",
+    description: "Dashboard page",
   };
-  
+
+  try {
+    const notes = await Note.aggregate([
+      {
+        $sort: {
+          createdAt: -1,
+        },
+      },
+      {
+        $match: { user: new mongoose.Types.ObjectId(req.user.id) },
+      },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Note.countDocuments().exec();
+
+    res.render("dashboard/index", {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard.ejs",
+      current: page,
+      pages: Math.ceil(count / perPage),
+    });
+  } catch (error) {
+    console.log(error);
+    next(error); 
+  }
+};
