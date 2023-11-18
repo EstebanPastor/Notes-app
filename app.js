5b5c4daeff39cec05b179cb67645d8fc522cbb27
EstebanPastor <estebanandrespastor@gmail.com>
2023-11-18 09:29:47 -0300

    feat: :sparkles: Add auth with google
    Now the user can log in/log out with his google acount

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 require("dotenv").config();
 
@@ -6,9 +9,23 @@ const expressLayouts = require("express-ejs-layouts");
 
 const connectDB = require("./server/config/db");
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-const app = express();
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+    }),
+    cookie: { maxAge: new Date(Date.now() + 604800000) },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,6 +41,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/index"));
+app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/dashboard"));
 
 // Handle 404
